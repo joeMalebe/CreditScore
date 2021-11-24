55fcad4cac262b887f49b2294f048fe6b55e2e16
joemalebe <jtekniq@gmail.com>
2021-11-24 20:09:37 +0200

    Create dependency module with retrofit dependency

---
+++ app/src/main/java/za/co/app/creditscore/ICreditScoreService.kt
@@ -0,0 +1,15 @@
+package za.co.app.creditscore
+
+import retrofit2.Response
+import retrofit2.http.GET
+import za.co.app.creditscore.model.CreditScoreModel
+
+interface ICreditScoreService {
+
+    companion object {
+        const val API_BASE_URL = "https://android-interview.s3.eu-west-2.amazonaws.com/"
+    }
+
+    @GET("endpoint.json")
+    suspend fun getCreditScoreDetails() : Response<CreditScoreModel>
+}
