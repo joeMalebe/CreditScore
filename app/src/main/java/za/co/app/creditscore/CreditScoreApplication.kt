44ebf975846a3ce967d7e3472fb01d9d79bf207d
joemalebe <jtekniq@gmail.com>
2021-11-24 22:42:29 +0200

    create application and run main activity

---
+++ app/src/main/java/za/co/app/creditscore/CreditScoreApplication.kt
@@ -0,0 +1,8 @@
+package za.co.app.creditscore
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CreditScoreApplication : Application() {
+}
--- app/src/main/java/za/co/app/creditscore/MainActivity.kt
+++ app/src/main/java/za/co/app/creditscore/MainActivity.kt
@@ -2,10 +2,36 @@ package za.co.app.creditscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var api : ICreditScoreService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        CoroutineScope(IO).launch {
+            val response = api.getCreditScoreDetails()
+            if(response.isSuccessful){
+                Log.d("Service call: ", response.body().toString())
+            } else {
+                Log.d("Failed Service call: ", response.message())
+            }
+        }
     }
 }
