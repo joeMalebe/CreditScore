12525414a6a30bbbebef546b506b7f7e7a163d3a
joemalebe <jtekniq@gmail.com>
2021-11-26 20:12:07 +0200

    bump up project to java 11 and android 12 and add compose

--- app/src/main/java/za/co/app/creditscore/ui/doughnut/DoughnutActivity.kt
+++ app/src/main/java/za/co/app/creditscore/ui/doughnut/DoughnutActivity.kt
@@ -3,10 +3,15 @@ package za.co.app.creditscore.ui.doughnut
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import za.co.app.creditscore.R
 import za.co.app.creditscore.ui.CreditScoreViewModel
+import za.co.app.creditscore.ui.domain.CreditScore
 
 @AndroidEntryPoint
 class DoughnutActivity : AppCompatActivity() {
@@ -16,11 +21,18 @@ class DoughnutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
 
+        
         viewModel.viewState.observe(this, { viewState -> renderViewState(viewState) })
         viewModel.loadCreditScore()
     }
 
+    @Composable
+    fun innerText(creditScore: CreditScore) {
+        Text(text = getString(R.string.androidx_startup))
+    }
+    
     private fun renderViewState(viewState: DoughnutViewState) {
         when (viewState) {
             is DoughnutViewState.Loading -> {
@@ -33,6 +45,9 @@ class DoughnutActivity : AppCompatActivity() {
 
             is DoughnutViewState.CreditScoreLoaded -> {
                 Log.d("#Main: ", "Data Loaded $viewState")
+                setContent {
+                    innerText(creditScore = viewState.creditScore)
+                }
             }
         }
     }
