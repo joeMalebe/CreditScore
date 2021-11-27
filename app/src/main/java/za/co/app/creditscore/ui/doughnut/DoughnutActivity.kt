b89b15d1b3f6ccf9c20daf905a311a27827b20c9
joemalebe <jtekniq@gmail.com>
2021-11-27 19:37:54 +0200

    separate doughnut ui from  activity

--- app/src/main/java/za/co/app/creditscore/ui/doughnut/DoughnutActivity.kt
+++ app/src/main/java/za/co/app/creditscore/ui/doughnut/DoughnutActivity.kt
@@ -1,10 +1,9 @@
 package za.co.app.creditscore.ui.doughnut
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.annotation.DimenRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -22,7 +21,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dagger.hilt.android.AndroidEntryPoint
 import za.co.app.creditscore.R
@@ -34,186 +32,12 @@ import za.co.app.creditscore.ui.doughnut.ui.theme.Typography
 @AndroidEntryPoint
 class DoughnutActivity : AppCompatActivity() {
 
-    val viewModel: CreditScoreViewModel by viewModels()
+    private val viewModel: CreditScoreViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel.viewState.observe(this, { viewState -> renderViewState(viewState) })
         viewModel.loadCreditScore()
-    }
-
-    @Composable
-    fun InnerText(creditScore: CreditScore) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Your credit score is", fontSize = 16.sp, textAlign = TextAlign.Center)
-            Text(
-                text = creditScore.score.toString(),
-                fontSize = 72.sp,
-                textAlign = TextAlign.Center,
-                color = colorResource(
-                    id = (R.color.colorAccent)
-                ),
-                fontWeight = FontWeight.Thin
-            )
-            Text(
-                text = "out of ${creditScore.targetScore}",
-                fontSize = 16.sp,
-                textAlign = TextAlign.Center
-            )
-        }
-    }
-
-    @Composable
-    fun ProgressBar(creditScore: CreditScore) {
-        CircularProgressIndicator(
-            progress = (creditScore.score.toFloat() / creditScore.targetScore.toFloat()),
-            Modifier
-                .size(dimensionResource(id = R.dimen.progress_bar_size))
-                .padding(dimensionResource(id = R.dimen.gutterSpaceHalf)),
-            color =  colorResource(
-                id = R.color.colorAccent
-            )
-        )
-    }
-
-    @Preview
-    @Composable
-    fun Preview() {
-        MainLayout(CreditScore(511, 700, null))
-    }
-
-    @Composable
-    fun Doughnut(creditScore: CreditScore) {
-        Box(contentAlignment = Alignment.Center , modifier = Modifier
-            .padding(dimensionResource(id = R.dimen.gutterSpace))) {
-            Box(
-                contentAlignment = Alignment.Center, modifier = Modifier
-                    .border(
-                        dimensionResource(id = R.dimen.outer_stroke_width), color = Color.Black,
-                        CircleShape
-                    )
-                    .clickable {
-                        startActivity(
-                            CreditReportActivity.getStartIntent(
-                                this@DoughnutActivity,
-                                creditScore
-                            )
-                        )
-                    }
-            ) {
-                ProgressBar(creditScore)
-                InnerText(creditScore)
-            }
-        }
-    }
-
-    @Composable
-    fun MainLayout(creditScore: CreditScore) {
-        Scaffold(topBar = {
-            TopAppBar(title = {
-                Text(text = stringResource(id = R.string.clear_score))
-            })
-        }, content = {
-            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally , modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    brush = Brush.linearGradient(
-                        listOf(
-                            colorResource(id = R.color.background),
-                            colorResource(id = R.color.background2),
-                            colorResource(id = R.color.background3),
-                            colorResource(id = R.color.background4),
-                            colorResource(id = R.color.silver)
-                        )
-                    )
-                )){
-
-                Doughnut(creditScore = creditScore)
-            }
-
-        })
-    }
-
-    @Composable
-    @Preview
-    fun Loading() {
-        Scaffold(topBar = {
-            TopAppBar(title = {
-                Text(text = stringResource(id = R.string.clear_score))
-            })
-        }, content = {
-            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally , modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    brush = Brush.linearGradient(
-                        listOf(
-                            colorResource(id = R.color.background),
-                            colorResource(id = R.color.background2),
-                            colorResource(id = R.color.background3),
-                            colorResource(id = R.color.background4),
-                            colorResource(id = R.color.silver)
-                        )
-                    )
-                )){
-
-                Box (contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()){
-                    CircularProgressIndicator(color = colorResource(id = R.color.colorPrimary), modifier = Modifier.size(
-                        dimensionResource(id = R.dimen.loader_size)))
-                }
-            }
-        })
-    }
-
-    @Composable
-    @Preview
-    fun Error() {
-        Scaffold(topBar = {
-            TopAppBar(title = {
-                Text(text = stringResource(id = R.string.clear_score))
-            })
-        }, content = {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.linearGradient(
-                            listOf(
-                                colorResource(id = R.color.background),
-                                colorResource(id = R.color.background2),
-                                colorResource(id = R.color.background3),
-                                colorResource(id = R.color.background4),
-                                colorResource(id = R.color.silver)
-                            )
-                        )
-                    )
-            ) {
-
-                MaterialTheme {
-                    Column {
-                        AlertDialog(title = {
-                            Text(
-                                text = stringResource(id = R.string.error_message),
-                                style = Typography.h6
-                            )
-                        }, text = {
-                            Text(
-                                text = stringResource(id = R.string.please_try_again_later),
-                                style = Typography.h6
-                            )
-                        }, onDismissRequest = {}, confirmButton = {
-                            Button(onClick = { finish() }) {
-                                Text("Ok")
-                            }
-                        })
-                    }
-                }
-            }
-        })
     }
 
     private fun renderViewState(viewState: DoughnutViewState) {
@@ -226,13 +50,22 @@ class DoughnutActivity : AppCompatActivity() {
 
             is DoughnutViewState.Error -> {
                 setContent {
-                    Error()
+                    Error { finish() }
                 }
             }
 
             is DoughnutViewState.CreditScoreLoaded -> {
                 setContent {
-                    MainLayout(creditScore = viewState.creditScore)
+                    MainLayout(
+                        creditScore = viewState.creditScore
+                    ) {
+                        startActivity(
+                            CreditReportActivity.getStartIntent(
+                                this,
+                                viewState.creditScore
+                            )
+                        )
+                    }
                 }
             }
         }
